475fb552044b8684ac89b8a8431400811497f6ab
AhmedAtef <aatef83156@gmail.com>
2023-08-25 23:19:36 +0300

    BotNavBar Dn

--- lib/Screens/app_starting.dart
+++ lib/Screens/app_starting.dart
@@ -1,21 +1,17 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:foodify/Screens/foodify_home.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+int orangeColor = 0xffFF774C;
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
-
 }
 
-
-
 class _SplashScreenState extends State<SplashScreen> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,12 +19,11 @@ class _SplashScreenState extends State<SplashScreen> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-
           Spacer(),
           Image(
-          image: const AssetImage("assets/images/Group.png"),
-            height: MediaQuery.of(context).size.height*0.5,
-            width: MediaQuery.of(context).size.width*0.5,
+            image: const AssetImage("assets/images/Group.png"),
+            height: MediaQuery.of(context).size.height * 0.5,
+            width: MediaQuery.of(context).size.width * 0.5,
           ),
           Spacer(),
           Padding(
@@ -39,22 +34,21 @@ class _SplashScreenState extends State<SplashScreen> {
                 Container(
                   child: TextButton(
                     onPressed: () {
-                      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context){
+                      Navigator.pushReplacement(context,
+                          MaterialPageRoute(builder: (context) {
                         return FoodifyHome();
                       }));
                     },
-                    child:Text("Skip",
+                    child: Text("Skip",
                         style: GoogleFonts.notoSansMultani(
-                            color: Color(0xffFF774C),
+                            color: Color(orangeColor),
                             fontSize: 18,
-                            fontWeight: FontWeight.bold)
-                    ),
+                            fontWeight: FontWeight.bold)),
                   ),
                 ),
               ],
             ),
           ),
-
         ],
       ),
     );
--- lib/Screens/foodify_home.dart
+++ lib/Screens/foodify_home.dart
@@ -1,33 +1,139 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:foodify/Screens/app_starting.dart';
+import 'package:foodify/Screens/BottomNavBarScreens/cart_screen.dart';
+import 'package:foodify/Screens/BottomNavBarScreens/home_screen.dart';
+import 'package:foodify/Screens/BottomNavBarScreens/notification_screen.dart';
+import 'package:foodify/Screens/BottomNavBarScreens/serach_screen.dart';
+import 'package:foodify/Screens/BottomNavBarScreens/wishlist_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class FoodifyHome extends StatelessWidget {
+int grayColor = 0xffDFE2E5;
+int grayColorDark = 0xffC6C9CC;
+int currentindex = 0;
+
+class FoodifyHome extends StatefulWidget {
   const FoodifyHome({Key? key}) : super(key: key);
 
   @override
+  State<FoodifyHome> createState() => _FoodifyHomeState();
+}
+
+List<dynamic> screens = [
+  HomeScreen(),
+  WishlistScreen(),
+  SearchScreen(),
+  NotificationScreen(),
+  CartScreen()
+];
+
+class _FoodifyHomeState extends State<FoodifyHome> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
         backgroundColor: Color(0xffF7F7FB),
-        bottomOpacity: 0,
-        primary: false,
-        shadowColor: Color(0xffF7FFB),
+        appBar: AppBar(
+          backgroundColor: Color(0xffF7F7FB),
+          shadowColor: Color(0xffF7FFB),
+          elevation: 0,
+          foregroundColor: Colors.white,
+          centerTitle: true,
+          title:Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Delivery to",
+                    style: GoogleFonts.notoSansMultani(
+                      decoration: TextDecoration. underline,
+                      color: Colors.black,
+                      fontSize: 12,
+                    ),
 
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 20,top: 30),
-            child: Text("Enjoy Delicious food",style: GoogleFonts.notoSansMultani(
-                color: Colors.black,
-                fontSize: 25,
-              fontWeight: FontWeight.bold
-            )
+                  ),
+                ],
+              ),
+              Text("Zagazig , Egypt",
+                style: GoogleFonts.notoSansMultani(
+                  color: Color(orangeColor),
+                  fontSize: 14,
+                ),
+
+              ),
+            ],
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(6.0),
+              child: Container(
+
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadiusDirectional.circular(16),
+                ),
+                child:Image(
+                  image: AssetImage("assets/images/image 2.png"),
+                  fit: BoxFit.cover,
+                )
+
+              ),
+            ),
+          ],
+          leading: Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: Container(
+
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadiusDirectional.circular(16),
+              ),
+              child: IconButton(
+                onPressed: (){},
+                icon: Icon(Icons.menu_rounded,color: Colors.black),
+              ),
+
             ),
           ),
-        ],
-      )
-    );
+        ),
+        body: screens[currentindex],
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined, color: Color(grayColorDark)),
+                label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.heart_broken_outlined,
+                    color: Color(grayColorDark)),
+                label: "Wishlist"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.search_outlined, color: Color(grayColorDark)),
+                label: "Search"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.notifications_outlined,
+                    color: Color(grayColorDark)),
+                label: "Notification"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_cart_outlined,
+                    color: Color(grayColorDark)),
+                label: "Cart"),
+          ],
+          currentIndex: currentindex,
+          type: BottomNavigationBarType.fixed,
+          iconSize: 28,
+          onTap: (int index) {
+            setState(() {
+              currentindex = index;
+              print(currentindex);
+            });
+          },
+          selectedFontSize: 16,
+          unselectedFontSize: 12,
+          fixedColor: Color(orangeColor),
+          selectedIconTheme: IconThemeData(
+            color: Color(orangeColor),
+            size: 32,
+          ),
+        ));
   }
 }
